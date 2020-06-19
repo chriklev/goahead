5f6569d83325d4e41b6adc83eec425be46815444
Christoffer Kleven Berg <cklevenb@gmail.com>
2020-06-19 16:52:19 +0200

    worked on MakeMove method

--- gogamegroup/gogamegroup.go
+++ gogamegroup/gogamegroup.go
@@ -1,12 +1,22 @@
 package gogamegroup
 
-// Game stores the functions and variables for a game instance
+import "log"
+
+const (
+	bsize = 9
+	black = 1
+	white = -1
+)
+
+// Game manages the methods and variables for a game instance
 type Game struct {
-	boards      [][9][9]int8
-	groups      []*group
-	boardgroups [9][9]*group
-	blackKills  uint8
-	whiteKills  uint8
+	board      [bsize][bsize]int8
+	groupboard [bsize][bsize]*group
+	boardhist  [][bsize][bsize]int8
+	groups     []*group
+	blackKills uint8
+	whiteKills uint8
+	turncolor  int8
 }
 
 type group struct {
@@ -17,10 +27,9 @@ type group struct {
 
 // NewGame returns a fresh Game structure, initialized for a new game
 func NewGame() (g Game) {
-	g.boards = make([][9][9]int8, 1, 100)
-	g.groups = make([]*group, 0)
 	g.blackKills = 0
 	g.whiteKills = 0
+	g.turncolor = black
 	return
 }
 
@@ -32,5 +41,58 @@ func (g *Game) newGroup(x, y int, color int8) {
 	// then add freedoms
 
 	g.groups = append(g.groups, &newgroup)
-	g.boardgroups[x][y] = &newgroup
+	g.groupboard[x][y] = &newgroup
+}
+
+// MakeMove registers a move in the game struct. Use x = y = -1 to pass
+func (g *Game) MakeMove(x, y int) bool {
+	// If move is pass
+	if x == -1 && y == -1 {
+		g.pass()
+		return true
+	}
+	// If the move is outside of board bounds
+	if x < 0 || x >= bsize || y < 0 || y >= bsize {
+		return false
+	}
+	// If the spot is not empty
+	if g.board[x][y] != 0 {
+		return false
+	}
+
+	// For each neighbour
+	dx, dy := 1, 0
+	for i := 0; i < 4; i++ {
+		nx, ny := x+dx, y+dy
+		// If current neighbour is oposite color
+		if g.board[nx][ny] == -g.turncolor {
+			g.removeFreedom(g.groupboard[nx][ny], x, y)
+		}
+		dx, dy = dy, -dx
+	}
+
+	return true
+}
+
+func (g *Game) pass() {
+	log.Fatalln("Pass function not implemented")
+}
+
+func (g *Game) removeFreedom(gr *group, x, y int) {
+	if len(gr.freedoms) == 1 {
+		g.removeGroup(gr)
+		return
+	}
+	for i, freedom := range gr.freedoms {
+		if freedom[0] == x && freedom[1] == y {
+			gr.freedoms[i] = gr.freedoms[len(gr.freedoms)-1]
+			gr.freedoms = gr.freedoms[:len(gr.freedoms)-1]
+			return
+		}
+	}
+	log.Fatalln("The group you tried to remove a freedom from did not have that freedom.")
+}
+
+func (g *Game) removeGroup(gr *group) {
+
 }
